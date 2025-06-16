829df74a9adfabe43f499d618f42b20f1dc20c88
Firagos <jemalfiragos@gmail.com>
2025-06-16 11:10:40 +0300

    creating insuranceplan model

---
+++ Server/models/insurancePlan.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const insurancePlanSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  premium: { type: Number, required: true }, // cost per period
+  coverageDetails: { type: String },
+  durationMonths: { type: Number, required: true }, // plan duration in months
+  createdAt: { type: Date, default: Date.now },
+});
+
+const InsurancePlan = mongoose.model("InsurancePlan", insurancePlanSchema);
+export default InsurancePlan;
