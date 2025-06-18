9859933b99f539eb02be8a7d6047da1bd330acbf
Firagos <jemalfiragos@gmail.com>
2025-06-18 08:50:27 +0300

    creating the plan model for insurance

---
+++ Server/models/policyAplication.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const policyApplicationSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    planId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Plan",
+      required: true,
+    },
+    documents: [String],
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+    remarks: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+const PolicyApplication = mongoose.model(
+  "PolicyApplication",
+  policyApplicationSchema
+);
+export default PolicyApplication;
