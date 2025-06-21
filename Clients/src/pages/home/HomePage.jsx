47750798a8f43527476cbc38b7f7fc16ad974cf9
Firagos <jemalfiragos@gmail.com>
2025-06-21 07:49:34 +0300

    adding feature section to it

--- Clients/src/pages/home/HomePage.jsx
+++ Clients/src/pages/home/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/navigation/Navbar'
+import Footer from '../../components/footer/Footer'
+import FeaturesSection from '../../components/cardSection/FeaturesSection'
 
 export default function HomePage() {
   return (
     <div>
         <Navbar />
+        <FeaturesSection />
+        <Footer />
     </div>
   )
 }
