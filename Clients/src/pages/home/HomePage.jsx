4c9068d01eeaf570f6f9a53c151985eb576b8d47
Firagos <jemalfiragos@gmail.com>
2025-06-21 11:12:06 +0300

    updating the home pages

--- Clients/src/pages/home/HomePage.jsx
+++ Clients/src/pages/home/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../../components/navigation/Navbar'
 import Footer from '../../components/footer/Footer'
 import FeaturesSection from '../../components/cardSection/FeaturesSection'
+import HeroSection from '../../components/heroSection/HeroSection'
 
 export default function HomePage() {
   return (
     <div>
         <Navbar />
+        <HeroSection />
         <FeaturesSection />
         <Footer />
     </div>
