64664fa45c8fa5d5303146d404adf33dbacf489f
Firagos <jemalfiragos@gmail.com>
2025-06-21 07:21:50 +0300

    creating navbar

---
+++ Clients/src/components/navigation/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow px-6 py-4 flex justify-between items-center">
+      <Link to="/" className="text-2xl font-bold text-blue-600">
+        InsureWise
+      </Link>
+      <div className="space-x-4">
+        <Link to="/#features" className="text-gray-600 hover:text-blue-600">
+          Features
+        </Link>
+        <Link to="/login" className="text-gray-600 hover:text-blue-600">
+          Login
+        </Link>
+        <Link
+          to="/register"
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+        >
+          Get Started
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
