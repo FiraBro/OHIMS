5316b7a17796ee540adc640c032534c61d38cbaf
Firagos <jemalfiragos@gmail.com>
2025-06-23 10:43:02 +0300

    adding the get user claim to it

--- Server/routes/claimRoutes.js
+++ Server/routes/claimRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   submitClaim,
   updateClaimStatus,
+  getUserClaims,
 } from "../controllers/claimController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
@@ -12,5 +13,7 @@ claimRouter.post("/submit", protect, upload.single("document"), submitClaim);
 
 // Admin approves/rejects claim
 claimRouter.put("/status/:id", protect, isAdmin, updateClaimStatus);
+
+claimRouter.get("/my-claims", protect, getUserClaims);
 
 export default claimRouter;
