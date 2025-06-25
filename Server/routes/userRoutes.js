005c482c499e7b552e6ab278ccc3ca40fb94faf8
Firagos <jemalfiragos@gmail.com>
2025-06-25 11:05:39 +0300

    adding the count route to it

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -3,11 +3,12 @@ import {
   getAllUser,
   getUser,
   deleteUser,
+  countUser,
 } from "../controllers/userController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 const userRouter = express.Router();
 
 userRouter.get("/all", protect, isAdmin, getAllUser);
 userRouter.delete("/delete/:id", protect, isAdmin, deleteUser);
-
+userRouter.get("/count", protect, isAdmin, countUser);
 export default userRouter;
