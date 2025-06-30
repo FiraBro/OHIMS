aba0c61c081058d7bc00a6490cba1cc80654e166
Firagos <jemalfiragos@gmail.com>
2025-06-30 17:03:58 +0300

    adding about section to it

--- Clients/src/pages/home/HomePage.jsx
+++ Clients/src/pages/home/HomePage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import Footer from '../../components/footer/Footer'
-import FeaturesSection from '../../components/cardSection/FeaturesSection'
-import HeroSection from '../../components/heroSection/HeroSection'
-import WhyChooseUs from '../../components/whChooseUs/WhyChooseUs'
-import GetStarted from '../../components/getStarted/GetStarted'
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import FeaturesSection from "../../components/cardSection/FeaturesSection";
+import HeroSection from "../../components/heroSection/HeroSection";
+import WhyChooseUs from "../../components/whChooseUs/WhyChooseUs";
+import GetStarted from "../../components/getStarted/GetStarted";
+import AboutSection from "../../components/About/AboutSection";
+import FAQSection from "../../components/Faq/FAQSection";
 
 export default function HomePage() {
   return (
     <div>
-        <HeroSection />
-        <FeaturesSection />
-        <WhyChooseUs />
-        <GetStarted />
+      <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <WhyChooseUs />
+      <GetStarted />
+      <FAQSection />
     </div>
-  )
+  );
 }
